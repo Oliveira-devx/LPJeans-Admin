a08bca5acc2d41e3647892b8655c78eddaeb01c8
pedro_dba <pedroh93942@gmail.com>
2026-07-31 20:57:46 +0000

    Adiciona suporte a SSL para conexao com banco em nuvem

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,15 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const precisaSSL = process.env.DB_HOST !== 'localhost';
+
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl: precisaSSL ? { rejectUnauthorized: false } : false,
 });
 
 pool.connect()
